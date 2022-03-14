7e5be22df20b6cee19eb8a8c451ff9e1d8b5b703
Gage Lasher <rsgage21@gmail.com>
2022-03-14 14:02:57 -0600

    commit

--- planit/server/models/Sprint.js
+++ planit/server/models/Sprint.js
@@ -2,5 +2,6 @@ import { Schema } from "mongoose";
 
 export const SprintSchema = new Schema({
     name: {type: String, required: true},
-    projectId: {type: Schema.Types.ObjectId, ref: 'Project'}
+    projectId: {type: Schema.Types.ObjectId, ref: 'Project'},
+    
 })
