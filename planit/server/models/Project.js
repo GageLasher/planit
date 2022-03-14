fe6fed2fdf7c2b94da7d4cfd7e31a18b1525619e
Gage Lasher <rsgage21@gmail.com>
2022-03-14 14:00:40 -0600

    commit

---
+++ planit/server/models/Project.js
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose";
+
+export const ProjectSchema = new Schema({
+    name: { type: String, required: true},
+    description: { type: String, required: true},
+    creatorId: {type: Schema.Types.ObjectId, ref: 'Profile', required: true}
+},
+ {timestamps: true, toJSON: {virtuals: true}}
+)
+
+
+ProjectSchema.virtual('creator', {
+    localField: 'creatorId',
+    foreignField: '_id',
+    justOne: true,
+    ref: 'Profile'
+})
---
+++ planit/server/models/Sprint.js
@@ -0,0 +1,6 @@
+import { Schema } from "mongoose";
+
+export const SprintSchema = new Schema({
+    name: {type: String, required: true},
+    projectId: {type: Schema.Types.ObjectId, ref: 'Project'}
+})
